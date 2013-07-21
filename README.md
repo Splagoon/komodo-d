32b4f957f03a2132e269ea6cc838ffec4dbffc71
Rob Kellett <rob@aorist.co>
2013-07-20 20:04:28 -0400

    Initial commit
    Basic syntax highlighting is supported

---
+++ README.md
@@ -0,0 +1,22 @@
+D for Komodo
+============
+
+What is it?
+-----------
+
+This project aims to add support for the [D programming language](http://dlang.org/) to [Komodo IDE](http://www.activestate.com/komodo-ide/) and [Komodo Edit](http://www.activestate.com/komodo-edit/).
+
+Currently, only basic syntax highlighting is supported.  Syntax checking and code intelligence are planned for the future.
+
+Installation
+------------
+
+To install D for Komodo in either Komodo IDE or Komodo Edit, simply download the "d_language-*.xpi" file and drag it onto Komodo, then restart Komodo.  You're good to go!
+
+Contributing
+------------
+If you'd like to help out, just fork this repository and make your changes.  When you're finished, issue a pull request and your changes will be reviewed.
+
+Licensing
+---------
+D for Komodo's source is made available through the terms of the [Mozilla Public License 2.0](http://www.mozilla.org/MPL/2.0/).
---
+++ components/koD_UDL_Language.py
@@ -0,0 +1,73 @@
+# Registers the D language in Komodo.
+
+import logging
+from koUDLLanguageBase import KoUDLLanguage
+
+log = logging.getLogger("koDLanguage")
+#log.setLevel(logging.DEBUG)
+
+def registerLanguage(registry):
+    log.debug("Registering language D")
+    registry.registerLanguage(KoDLanguage())
+
+class KoDLanguage(KoUDLLanguage):
+
+    # ------------ Komodo Registration Information ------------ #
+
+    name = "D"
+    lexresLangName = "D"
+    _reg_desc_ = "%s Language" % name
+    _reg_contractid_ = "@activestate.com/koLanguage?language=%s;1" % name
+    _reg_categories_ = [("komodo-language", name)]
+    _reg_clsid_ = "6abe481f-5de4-466f-a192-35f24d6dff26"
+    defaultExtension = '.d'
+
+    # ------------ Commenting Controls ------------ #
+
+    commentDelimiterInfo = {
+        "line": [
+                '//',   # C-style one line comments
+                ],
+        "block": [
+                ('/*', '*/')   # C-style block comments
+                #('/+', '+/')   # D-style nested comments
+                ],
+    }
+
+    # ------------ Indentation Controls ------------ #
+
+    # To support automatic indenting and dedenting after "{([" and "})]"
+    supportsSmartIndent = "brace"
+    # Other smart indenting types are:
+    #   'text', 'python', 'XML' and 'keyword'
+
+    # Indent/dedent after these words.
+    _indenting_statements = ['case']
+    #_dedenting_statements = ['return', 'break', 'continue']
+
+    # ------------ Sub-language Controls ------------ #
+
+    #Check: Update 'lang_from_udl_family' as appropriate for your
+    #      lexer definition. There are four UDL language families:
+    #           M (markup), i.e. HTML or XML
+    #           CSL (client-side language), e.g. JavaScript
+    #           SSL (server-side language), e.g. Perl, PHP, Python
+    #           TPL (template language), e.g. RHTML, Django, Smarty
+    #      'lang_from_udl_family' maps each UDL family code (M,
+    #      CSL, ...) to the sub-language name in your language.
+    #      Some examples:
+    #        lang_from_udl_family = {   # A PHP file can contain
+    #           'M': 'HTML',            #   HTML
+    #           'SSL': 'PHP',           #   PHP
+    #           'CSL': 'JavaScript',    #   JavaScript
+    #        }
+    #        lang_from_udl_family = {   # An RHTML file can contain
+    #           'M': 'HTML',            #   HTML
+    #           'SSL': 'Ruby',          #   Ruby
+    #           'CSL': 'JavaScript',    #   JavaScript
+    #           'TPL': 'RHTML',         #   RHTML template code
+    #        }
+    #        lang_from_udl_family = {   # A plain XML can just contain
+    #           'M': 'XML',             #   XML
+    #        }
+    lang_from_udl_family = {'SSL': 'D'}
